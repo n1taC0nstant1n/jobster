d810df5d86f50464e6793f89327d91f5182fd3ca
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-23 16:10:02 +0300

    431 Style Landing Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Landing from "./pages/Landing";
-import styled from "styled-components";
+//import styled from "styled-components";
 
-const Button = styled.button`
-  background: red;
-  color: #fff;
-  font-size: 2rem;
-`;
-
-const SecondButton = styled.button`
-  background: #0000ff;
-  color: #fff;
-  font-size: 3rem;
-`;
 function App() {
   return (
     <div>
-      <SecondButton>Click Me</SecondButton>
-      <Button>Click Me</Button>
       <Landing />
     </div>
   );

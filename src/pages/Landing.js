b27b5c75623edf70f5b5253b9dfa1425a513e777
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-23 15:17:04 +0300

    428 Landing Page Setup and Structure

---
+++ src/pages/Landing.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/*info*/}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+          </p>
+          <button className="btn btn-herro">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;

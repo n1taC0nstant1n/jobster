69103a58df4f6e94c4ee4849825c6ca7b2777561
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-23 18:29:28 +0300

    435 Setup Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Landing from "./pages/Landing";
+import { Landing, Error, Register, Dashboard } from "./pages";
 //import styled from "styled-components";
 
 function App() {
   return (
     <div>
+      <Error />
       <Landing />
     </div>
   );

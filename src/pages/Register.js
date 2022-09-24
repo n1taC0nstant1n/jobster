28feb89cf532c72910e2e41b8db9102af0bd7b07
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-24 19:47:39 +0300

    455 Programmatically Navigate To Dashboard

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,6 +6,7 @@ import { FormRow } from "../components";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: "",
@@ -17,7 +18,7 @@ function Register() {
   const [values, setValues] = useState(initialState);
   const { user, isLoading } = useSelector((store) => store.user);
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -43,6 +44,14 @@ function Register() {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+  }, [navigate, user]);
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>

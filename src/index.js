7c2a76f589b98b2876c9da030011a6542f94e2c6
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-24 11:20:57 +0300

    446 User Slice - Setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
 );
